3193cee5bcc913522264edc29a190c551fa35898
Rizqy  H <rizqy@dicoding.com>
2021-09-01 17:59:27 +0700

    Create test Generate Teams message payload

---
+++ __tests__/TeamsMessageGenerator.test.js
@@ -0,0 +1,16 @@
+import { describe, expect, it } from '@jest/globals';
+import commentStub from './comment.stub';
+import TeamsMessageGenerator from '../src/TeamsMessageGenerator';
+
+describe('Generate Teams message payload', () => {
+    it('Should have basic metadata', () => {
+        const comment = new FigmaComment(commentStub);
+        const message = new TeamsMessageGenerator(comment);
+
+        expect(message.toPayload()).toHaveProperty('@type', 'MessageCard');
+        expect(message.toPayload()).toHaveProperty('@context', 'https://schema.org/extensions');
+        expect(message.toPayload()).toHaveProperty('themeColor', 'FF0000');
+        expect(message.toPayload()).toHaveProperty('title', 'New comment');
+        expect(message.toPayload()).toHaveProperty('summary', 'New comment summary');
+    });
+});
