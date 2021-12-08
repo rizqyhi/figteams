2f25c385eafbc91e60a4fd821e86c6a33de2f5e1
Rizqy  H <rizqy@dicoding.com>
2021-12-08 18:33:35 +0700

    Move Teams webhook URL to env variable

--- tests/SendTeamsNotification.test.js
+++ tests/SendTeamsNotification.test.js
@@ -1,17 +1,18 @@
+import dotenv from 'dotenv';
 import { describe, expect, it } from '@jest/globals';
 import fetch from 'node-fetch';
 import commentStub from '../src/__tests__/commentWebhook.stub.js';
 import FigmaComment from '../src/FigmaComment.js';
 import TeamsMessageGenerator from '../src/TeamsMessageGenerator.js';
 
-const TEAMS_WEBHOOK_URL = 'https://presentologics.webhook.office.com/webhookb2/3fbda82a-fa24-47af-952f-9f439aea0554@a266a087-622d-47cc-8c26-c15071fe3650/IncomingWebhook/18e493db4f464296b18dc93294179ebf/10a7860f-b1ad-4477-99f7-11228c24a1cf';
+dotenv.config()
 
 describe('Send Teams notification', () => {
     it('Should send the message succesfully', async () => {
         const comment = new FigmaComment(commentStub);
         const message = new TeamsMessageGenerator(comment);
 
-        const response = await fetch(TEAMS_WEBHOOK_URL, {
+        const response = await fetch(process.env.TEAMS_WEBHOOK_URL, {
             method: 'post',
             body: JSON.stringify(message.toPayload()),
             headers: {'Content-Type': 'application/json'}
