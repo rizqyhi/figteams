2e316d8a9bf133ac9a990c7f31a8320c65b6d9dd
Rizqy  H <rizqy@dicoding.com>
2021-12-08 18:08:03 +0700

    Fix async await test

--- tests/SendTeamsNotification.test.js
+++ tests/SendTeamsNotification.test.js
@@ -7,16 +7,16 @@ import TeamsMessageGenerator from '../src/TeamsMessageGenerator.js';
 const TEAMS_WEBHOOK_URL = 'https://presentologics.webhook.office.com/webhookb2/3fbda82a-fa24-47af-952f-9f439aea0554@a266a087-622d-47cc-8c26-c15071fe3650/IncomingWebhook/18e493db4f464296b18dc93294179ebf/10a7860f-b1ad-4477-99f7-11228c24a1cf';
 
 describe('Send Teams notification', () => {
-    it('Should send the message succesfully', () => {
+    it('Should send the message succesfully', async () => {
         const comment = new FigmaComment(commentStub);
         const message = new TeamsMessageGenerator(comment);
 
-        const response = fetch(TEAMS_WEBHOOK_URL, {
+        const response = await fetch(TEAMS_WEBHOOK_URL, {
             method: 'post',
             body: JSON.stringify(message.toPayload()),
             headers: {'Content-Type': 'application/json'}
-        })
+        });
 
-        expect(response.ok).toBe(true);
+        await expect(response.ok).toBe(true);
     });
 });
