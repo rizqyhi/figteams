5579a43fa59867b94a1a4bbfc2997fee652687d3
Rizqy  H <rizqy@dicoding.com>
2022-01-03 18:05:06 +0700

    Wrap in a try-catch block

--- api/webhook.js
+++ api/webhook.js
@@ -3,13 +3,17 @@ import TeamsMessageGenerator from '../src/TeamsMessageGenerator.js';
 import { sendMessage } from '../src/TeamsMessageSender.js';
 
 export default async function handler(request, response) {
-    const comment = new FigmaComment(request.body);
-    const message = new TeamsMessageGenerator(comment);
-    const webhookResponse = await sendMessage(message);
-    
-    if (webhookResponse.ok) {
-        response.status(200).send('OK');
-    } else {
+    try {
+        const comment = new FigmaComment(request.body);
+        const message = new TeamsMessageGenerator(comment);
+        const webhookResponse = await sendMessage(message);
+        
+        if (webhookResponse.ok) {
+            response.status(200).send('OK');
+        } else {
+            response.status(400).send('FAILED');
+        }
+    } catch (error) {
         response.status(400).send('FAILED');
     }
 }
