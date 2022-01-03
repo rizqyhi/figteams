6e4ddba3940542dace5e5a9abcaef9aa1695f6cd
Rizqy  H <rizqy@dicoding.com>
2022-01-03 17:31:51 +0700

    Create function handler

--- api/handler.js
+++ api/handler.js
@@ -1,4 +1,15 @@
-module.exports = (req, res) => {
-    console.log(req.body)
-    res.send('OK')
-};
+import FigmaComment from '../src/FigmaComment';
+import TeamsMessageGenerator from '../src/TeamsMessageGenerator';
+import { sendMessage } from '../src/TeamsMessageSender';
+
+export default function handler(request, response) {
+    const comment = new FigmaComment(request.body);
+    const message = new TeamsMessageGenerator(comment);
+    const response = await sendMessage(message);
+    
+    if (response.ok) {
+        response.status(200).send('OK');
+    } else {
+        response.status(400).send('FAILED');
+    }
+}
